ef019cea5474329c070aec1863390a67ba171a5e
Lollators <gm.lollators@gmail.com>
2020-03-08 23:39:01 -0400

    Fixed mainscreen to utilize a scrollable tableview. Added floating add product button with sample functionality

--- app/src/main/java/com/github/lollators/myDiabetesSupplies/MainScreen.java
+++ app/src/main/java/com/github/lollators/myDiabetesSupplies/MainScreen.java
@@ -1,9 +1,14 @@
 package com.github.lollators.myDiabetesSupplies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -21,6 +26,33 @@ public class MainScreen extends AppCompatActivity {
 
     public void refresh(android.view.View view){
 
+
+
+
+    }
+
+    public void add(android.view.View view){
+        TableLayout myTable = findViewById(R.id.table);
+        TableRow newRow = new TableRow(this);
+        newRow.setBackground(ContextCompat.getDrawable(this, R.drawable.tableborder));
+        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+
+
+        newRow.setLayoutParams(lp);
+        TextView name = new TextView(this);
+        TextView category = new TextView(this);
+        TextView expirationDate = new TextView(this);
+        TextView box = new TextView(this);
+        name.setText("test");
+        category.setText("Sites");
+        expirationDate.setText("02/20/2021");
+        box.setText("A");
+
+        newRow.addView(name);
+        newRow.addView(category);
+        newRow.addView(expirationDate);
+        newRow.addView(box);
+        myTable.addView(newRow);
     }
 
 
